5f3eb3509cca432877572b9636959479b97a80cb
to-lutz <tobias.lutz07@gmail.com>
2024-11-26 20:11:25 +0100

    Line Numbering functionality

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,70 @@
-let currentLine = 1; // Current Line for Editor
+let currentLine = 1;
 let currentLineElem = document.querySelector(".editor-line-text-selected");
 let lineNumElem = document.querySelector(".editor-line-numbers");
+let textAreaElem = document.querySelector(".editor-text-input");
 
-document.querySelector(".editor-text-input").addEventListener("keypress", (e) => {
+textAreaElem.addEventListener("keypress", (e) => {
     if (e.key == "Enter") {
         currentLineElem.classList.remove("editor-line-text-selected");
 
         let lineNum = document.createElement("div");
         lineNum.classList.add("editor-line-text");
-        lineNum.classList.add("editor-line-text-selected");
-        lineNum.innerHTML = currentLine+1;
+        lineNum.id = "lineNum" + (currentLine+1);
+        lineNum.innerHTML = currentLine + 1;
         lineNumElem.appendChild(lineNum);
 
         currentLineElem = lineNum;
-
         currentLine++;
+
+        // Refresh all Line Numbers
+        refreshLineNumbers();
     }
 });
+
+textAreaElem.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+        if (getTextAtLine(currentLine) <= 1 && currentLine > 1) {
+            document.querySelector("#lineNum" + currentLine).remove();
+            currentLineElem = document.querySelector("#lineNum" + (currentLine-1));
+            currentLine--;
+
+            // Refresh all Line Numbers
+            refreshLineNumbers();
+        }
+    }
+});
+
+textAreaElem.addEventListener("click", (e) => { // Click change cursor
+    currentLineElem.classList.remove("editor-line-text-selected");
+
+    currentLine = getCurrentLineNumber();
+    currentLineElem = document.querySelector("#lineNum" + currentLine);
+    refreshLineNumbers();
+});
+
+function getCurrentLineNumber() {
+    let cursorPosition = textAreaElem.selectionStart;
+    let text = textAreaElem.value;
+    let lines = text.slice(0, cursorPosition).split("\n");
+    return lines.length;
+}
+
+function getTextAtLine(line) {
+    let cursorPosition = textAreaElem.selectionStart;
+    let text = textAreaElem.value;
+    let lines = text.slice(0, cursorPosition).split("\n");
+    return lines[line - 1];
+}
+
+function refreshLineNumbers() {
+    document.querySelectorAll(".editor-line-text").forEach((e, index) => {
+        e.id = "lineNum" + (index+1);
+        e.innerHTML = (index+1);
+
+        if (index + 1 == currentLine) {
+            e.classList.add("editor-line-text-selected");
+        } else {
+            e.classList.remove("editor-line-text-selected");
+        }
+    });
+}
